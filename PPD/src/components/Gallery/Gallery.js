03b2fa4a6427af489755c5782a8dd3393383e330
alphiek <43937504+alphiek@users.noreply.github.com>
2019-11-24 01:51:49 +0200

    Add services, update gallery

--- PPD/src/components/Gallery/Gallery.js
+++ PPD/src/components/Gallery/Gallery.js
@@ -4,38 +4,34 @@ import { Divider } from "../Utils/Divider";
 import { colors } from "../Utils/colors";
 
 export const Gallery = () => {
-  const [expand, setExpanded] = useState("0");
+  const [showing, setShowing] = useState("0");
+  const isShowing = id => (showing === id ? true : false);
 
   const toggleActive = e => {
-    console.log(e.target.id);
-    e.target.id === expand ? setExpanded("0") : setExpanded(e.target.id);
+    e.target.id === showing ? setShowing("0") : setShowing(e.target.id);
   };
-
-  const isExpanded = id => (expand === id ? true : false);
 
   return (
     <Section id="gallery">
       <GridParent>
-        <ProjectOne isActive={isExpanded("1")} onClick={toggleActive} id="1">
+        <ProjectOne isActive={isShowing("1")} onClick={toggleActive} id="1">
           One
         </ProjectOne>
-        <ProjectTwo isActive={isExpanded("2")} onClick={toggleActive} id="2">
+        <ProjectTwo isActive={isShowing("2")} onClick={toggleActive} id="2">
           Two
         </ProjectTwo>
-        <TitleWrapper state={expand}>
+        <TitleWrapper state={showing}>
           <div>
             <h2>Project Gallery</h2>
             <Divider />
             <p>
-              Check out some of our recent projects around the Paphos District
+              Check out some of our recent projects
+              <br />
+              around the Paphos District
             </p>
           </div>
         </TitleWrapper>
-        <ProjectThree
-          isActive={isExpanded("3")}
-          onClick={toggleActive}
-          id="3"
-        >
+        <ProjectThree isActive={isShowing("3")} onClick={toggleActive} id="3">
           Four
         </ProjectThree>
       </GridParent>
@@ -45,7 +41,8 @@ export const Gallery = () => {
 
 const Section = styled.section`
   width: auto;
-  margin: 15%;
+  margin-top: 5%;
+  padding: 5% 15%;
 `;
 
 const GridParent = styled.div`
@@ -68,22 +65,18 @@ const Flex = styled.div`
   grid-row: ${props => (props.isActive ? "span 2" : "span 1")};
 `;
 
-const ProjectOne = styled(Flex)`
-`;
+const ProjectOne = styled(Flex)``;
 
-const ProjectTwo = styled(Flex)`
-`;
+const ProjectTwo = styled(Flex)``;
 
-const ProjectThree = styled(Flex)`
-`;
+const ProjectThree = styled(Flex)``;
 
 const TitleWrapper = styled.div`
-  grid-column: ${props => props.state !== '0' ? 'span 4' : 'span 2'};
-  order: ${props => props.state !== '0' ? '4' : ''};
+  grid-column: ${props => (props.state !== "0" ? "span 4" : "span 2")};
+  order: ${props => (props.state !== "0" ? "4" : "")};
   grid-row: span 1;
   display: flex;
   border: 6px solid ${colors.orange};
-  justify-content: center;
   align-items: center;
   padding: 0 10% 0 15%;
 `;
--- PPD/src/components/Services/Services.js
+++ PPD/src/components/Services/Services.js
@@ -4,12 +4,19 @@ import { Divider } from "../Utils/Divider";
 
 export const Services = () => {
   return (
-    <Section id='services'>
+    <Section id="services">
       <div>
         <h2>Our Services and Skills</h2>
         <Divider />
         <p>Take a look at our skills and services</p>
       </div>
+      <FlexContainer>
+        <FlexItem>Service 1</FlexItem>
+        <FlexItem>Service 1</FlexItem>
+        <FlexItem>Service 1</FlexItem>
+        <FlexItem>Service 1</FlexItem>
+        <FlexItem>Service 1</FlexItem>
+      </FlexContainer>
     </Section>
   );
 };
@@ -17,12 +24,23 @@ export const Services = () => {
 const Section = styled.section`
   width: 100%;
   height: 100vh;
+  background-color: #fafafa;
+  padding: 10%;
 `;
 
-const TextWrapperRight = styled.div`
-  margin: 10% 10%;
-  text-align: right;
+const FlexContainer = styled.div`
   display: flex;
-  justify-content: flex-end;
-  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: center;
+  border: 1px solid black;
+  margin-top: 4rem;
+`;
+
+const FlexItem = styled.div`
+  width: 20%;
+  display: flex;
+  min-width: 200px;
+  justify-content: center;
+  align-items: center;
+  height: 300px;
 `;
